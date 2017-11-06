95eb43eb0c39e4779909ceaf17612a6949cb7fa3
mlitman <sahithireddy.golipalli@cuw.edu>
2017-11-06 04:28:44 +0300

    initial commit

---
+++ app/src/main/java/com/example/pop/csc537_fall2017_hw2_template/MainActivity.java
@@ -0,0 +1,191 @@
+package com.example.pop.csc537_fall2017_hw2_template;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.pop.csc537_fall2017_hw2_template.databinding.ActivityMainBinding;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private static final char ADDITION = '+';
+
+
+    private char CURRENT_ACTION;
+
+    private double valueOne = Double.NaN;
+    private double valueTwo;
+
+    private DecimalFormat decimalFormat;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        HugeInteger L = new HugeInteger();
+
+//        L.add(1);
+//        L.show();
+//        L.add(2);
+//        L.show();
+//        L.add(3);
+//        L.show();
+//        L.add(4);
+//        L.show();
+//        L.add(5);
+//        L.show();
+//        L.add(6);
+//        L.show();
+//        L.add(7);
+//        L.show();
+//        L.add(8);
+//        L.show();
+//        L.add(9);
+//        L.show();
+
+       decimalFormat = new DecimalFormat("#.##########");
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+//        binding.buttonDot.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                binding.editText.setText(binding.editText.getText() + ".");
+//            }
+//        });
+
+        binding.buttonZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "0");
+            }
+        });
+
+        binding.buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "1");
+            }
+        });
+
+        binding.buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "2");
+            }
+        });
+
+        binding.buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "3");
+            }
+        });
+
+        binding.buttonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "4");
+            }
+        });
+
+        binding.buttonFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "5");
+            }
+        });
+
+        binding.buttonSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "6");
+            }
+        });
+
+        binding.buttonSeven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "7");
+            }
+        });
+
+        binding.buttonEight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "8");
+            }
+        });
+
+        binding.buttonNine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "9");
+            }
+        });
+
+        binding.buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                computeCalculation();
+                CURRENT_ACTION = ADDITION;
+                binding.infoTextView.setText(decimalFormat.format(valueOne) + "+");
+                binding.editText.setText(null);
+            }
+        });
+
+
+
+
+
+
+        binding.buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                computeCalculation();
+                binding.infoTextView.setText(binding.infoTextView.getText().toString() +
+                        decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+                valueOne = Double.NaN;
+                CURRENT_ACTION = '0';
+            }
+        });
+
+        binding.buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.editText.getText().length() > 0) {
+                    CharSequence currentText = binding.editText.getText();
+                    binding.editText.setText(currentText.subSequence(0, currentText.length()-1));
+                }
+                else {
+                    valueOne = Double.NaN;
+                    valueTwo = Double.NaN;
+                    binding.editText.setText("");
+                    binding.infoTextView.setText("");
+                }
+            }
+        });
+    }
+
+    private void computeCalculation() {
+        if(!Double.isNaN(valueOne)) {
+            valueTwo = Double.parseDouble(binding.editText.getText().toString());
+            binding.editText.setText(null);
+
+            if(CURRENT_ACTION == ADDITION)
+                valueOne = this.valueOne + valueTwo;
+        }
+        else {
+            try {
+                valueOne = Double.parseDouble(binding.editText.getText().toString());
+            }
+            catch (Exception e){}
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pop/csc537_fall2017_hw2_template/Node.java
@@ -0,0 +1,9 @@
+package com.example.pop.csc537_fall2017_hw2_template;
+public class Node {
+    Node next = null;
+    int data;
+
+    public Node(int d){
+        data = d;
+    }
+}
